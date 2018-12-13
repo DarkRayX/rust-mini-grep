35cc51f7345185c0fe3e5fa2541593673a9e1eda
Ivan <DarkRay2006@gmail.com>
2018-12-13 18:11:56 +0300

    initial commit

---
+++ tests/one_result.rs
@@ -0,0 +1,12 @@
+extern crate minigrep;
+use minigrep::*;
+
+#[test]
+fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+}
